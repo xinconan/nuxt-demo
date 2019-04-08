f85929323ea628e084fed183c506fe76bd4df0e3
xinconan <xinconan2@sina.com>
2019-04-08 21:43:40 +0800

    feat: 登录、注册页面

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <my-menu/>
+      </el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col></el-col>
+    </el-row>
+  </section>
+</template>
+
+<script>
+import MyMenu from '~/components/index/menu.vue'
+import Life from '~/components/index/life.vue'
+
+export default {
+  components: {
+    MyMenu,
+    Life
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/index/index.scss";
+</style>
